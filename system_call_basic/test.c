fe209cfaab467b952c1e6a06f6c777cb4c262617
Rasmi Lamichhane <rala8730@colorado.edu>
2017-09-14 23:00:50 -0600

    adding test for simple_add

--- system_call_basic/test.c
+++ system_call_basic/test.c
@@ -3,10 +3,16 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <signal.h>
-
+#include<stdlib.h>
 int  main()
 {          
  int mycall=syscall(332);
- //printk("call:",syscall); 
+ 
+ int number1=1;
+ int number2=2;
+ int *result=malloc(sizeof(int));
+ int mycall_simple_one=syscall(333,number1,number2,&result);
+// printk(KERN_ALERT,"mysyscall)simple_one: %d\n",mycall_simple); 
+//printk("call:",syscall); 
  return 0;
 }
