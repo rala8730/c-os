cb70727ed83c546f3fc1fa2ec6d52f59699fcf8f
rala8730 <rala8730@colorado.edu>
2017-10-14 17:02:18 -0600

    finish simple char

--- pa2/simple_char_driver.c
+++ pa2/simple_char_driver.c
@@ -8,9 +8,14 @@
 #include<asm/uaccess.h>
 
 #define BUFFER_SIZE 1024
+//#define major_number 240
+//#define name_of_driver "simple_char_device"
 
 /* Define device_buffer and other global data structures you will need here */
 
+int opencount=0;
+int closecount=0;
+char device_buffer[BUFFER_SIZE];
 
 ssize_t simple_char_driver_read (struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
 {
@@ -18,6 +23,12 @@ ssize_t simple_char_driver_read (struct file *pfile, char __user *buffer, size_t
     /* length is the length of the userspace buffer*/
     /* offset will be set to current position of the opened file after read*/
     /* copy_to_user function: source is device_buffer and destination is the userspace buffer *buffer */
+	//buffer=destination
+	//source= device_buffer
+	//size=length
+	printk(KERN_ALERT,"copying data");
+	copy_to_user(buffer,device_buffer,length);
+	printk(KERN_ALERT,"copied data: %d",length);
     
     return 0;
 }
@@ -30,6 +41,10 @@ ssize_t simple_char_driver_write (struct file *pfile, const char __user *buffer,
     /* length is the length of the userspace buffer*/
     /* current position of the opened file*/
     /* copy_from_user function: destination is device_buffer and source is the userspace buffer *buffer */
+	printk(KERN_ALERT,"writing data");	
+	*offset = strlen(&device_buffer[0]);	
+	copy_from_user(buffer, device_buffer,length);
+	printk(KERN_ALERT,"written data: %d",length);
     
     return length;
 }
@@ -42,22 +57,36 @@ int simple_char_driver_open (struct inode *pinode, struct file *pfile)
     
     /* print to the log file that the device is opened and also print the number of times this device has been opened until now*/
     //pdf says only print the count not the log file
-    count++;
-    printk(KERN_ALERT"number of times device has been opened:%d\n",count);
+    opencount++;
+    printk(KERN_ALERT"number of times device has been opened:%d\n",opencount);
     return 0;
 }
 
 int simple_char_driver_close (struct inode *pinode, struct file *pfile)
 {
     /* print to the log file that the device is closed and also print the number of times this device has been closed until now*/
-    count++;
-    printk(KERN_ALERT"number of times device has been closed:%d\n",count);
+    closecount++;
+    printk(KERN_ALERT"number of times device has been closed:%d\n",closecount);
     return 0;
 }
 
-loff_t simple_char_driver_seek (struct file *pfile, loff_t offset, int whence)
+loff_t simple_char_driver_llseek (struct file *pfile, loff_t offset, int whence)
 {
     /* Update open file position according to the values of offset and whence */
+	//loff_t f_pos;
+	switch (whence){
+		//the current position is set to the value of the offset.
+		case 0: 
+		pfile->f_pos=offset;
+		break;
+		//the current position is incremented by offset bytes 
+		case 1:
+		pfile->f_pos=pfile->f_pos+offset;
+		break;
+		//the current position is set to offset bytes before the end of the file.
+		case 2:pfile->f_pos=device_buffer-offset;
+		break;	
+	}
     return 0;
 }
 
@@ -67,8 +96,8 @@ struct file_operations simple_char_driver_file_operations = {
     /* add the function pointers to point to the corresponding file operations. look at the file fs.h in the linux souce code*/
     //function pointers (.open, .close, .llseek, .read, .write) are pointing to the five functions for these operations.
     .open=simple_char_driver_open,
-    .close=simple_char_driver_close,
-    .llseek=simple_char_driver_seek,
+    .release=simple_char_driver_close,
+    .llseek=simple_char_driver_llseek,
     .read=simple_char_driver_read,
     .write=simple_char_driver_write,
 };
@@ -77,7 +106,9 @@ static int simple_char_driver_init(void)
 {
     /* print to the log file that the init function is called.*/
     /* register the device */
-    register_chrdev(major_number,name_of_driver,*simple_char_driver_file_operations);
+	printk(KERN_ALERT "Entering simple character device\n");
+	void *__kmalloc(size_t size, gfp_t GFP_KERNAL);
+    	register_chrdev(240,"simple_char_device",&simple_char_driver_file_operations);
     return 0;
 }
 
@@ -85,7 +116,9 @@ static int simple_char_driver_exit(void)
 {
     /* print to the log file that the exit function is called.*/
     /* unregister  the device using the register_chrdev() function. */
-    unregister_chrdev(major_number,name_of_driver);
+	printk(KERN_ALERT "Exiting simple character device \n");
+	unregister_chrdev(240,"simple_char_device");
+	void kfree(device_buffer);
     return 0;
 }
 
